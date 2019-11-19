15c4654877b55e8c755ae579088901043bf378a5
amichael497 <52112106+amichael497@users.noreply.github.com>
2019-11-19 13:55:44 -0600

    Add files via upload

--- Table.java
+++ Table.java
@@ -3,21 +3,26 @@ public class Table {
 	private boolean open;
 	public int seating;
 	public int tableNumber;
+	public int numberSeated;
 	
 	public Table(int seating, int tableNumber) {
 		this.seating = seating;
 		this.tableNumber = tableNumber;
+		numberSeated = 0;
+		open = true;
 	}
 	
 	public boolean checkAvailability() {
-		return this.open;
+		return open;
 	}
 	
-	public void useTable() {
-		this.open = false;
+	public void useTable(int guests) {
+		open = false;
+		numberSeated = guests;
 	}
 	
 	public void openTable() {
-		this.open = true;
+		open = true;
+		numberSeated = 0;
 	}
 }
